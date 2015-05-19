90d4f912b867c7519a4fac88a38b8135286b2b82
Christian Höfer <info@ch-webdev.de>
2015-05-19 16:42:06 +0200

    Dokumentation entfernt

--- doc/elementlist.js
+++ doc/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","loci\\api\\API"],["c","loci\\api\\lib\\Aktion"],["c","loci\\api\\lib\\Kampagne"],["c","loci\\api\\lib\\Kunde"],["c","loci\\api\\lib\\Mandant"],["c","loci\\api\\lib\\Partner"],["c","loci\\api\\lib\\Teilnehmer"]];
+ApiGen.elements = [["c","loci\\api\\API"],["c","loci\\api\\lib\\Aktion"],["c","loci\\api\\lib\\ApiException"],["c","loci\\api\\lib\\Kampagne"],["c","loci\\api\\lib\\Kunde"],["c","loci\\api\\lib\\Mandant"],["c","loci\\api\\lib\\Partner"],["c","loci\\api\\lib\\Teilnehmer"]];
